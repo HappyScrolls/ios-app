beb5bf55b11d65af1228fa8e14e522e0a5cf11a5
Yeji <mnbvcxzyj@gmail.com>
2024-11-13 23:49:44 +0900

    initial setting

---
+++ Togethery/TogetheryTests/TogetheryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TogetheryTests.swift
+//  TogetheryTests
+//
+//  Created by 김예지 on 11/13/24.
+//
+
+import Testing
+@testable import Togethery
+
+struct TogetheryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
